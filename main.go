48f80dcacccc722dec27250b283221030847529b
Davide Giovannini <giovannini.davide90@gmail.com>
2016-11-20 15:25:24 +0100

    Refactored main

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image"
 	"image/png"
 	"os"
 )
@@ -17,7 +18,112 @@ const (
 )
 
 func main() {
+	args := parse_command_args()
 
+	//
+	data := make(chan byte)
+
+	//Read the Png
+	image_file, err := os.Open(args.image_path)
+	if err != nil {
+		panic("Error while opening image!")
+	}
+	defer image_file.Close()
+
+	image, err := png.Decode(image_file)
+	if err != nil {
+		panic("Error while decoding png!")
+	}
+
+	if args.wants_decode {
+		call_decode(args, image, data)
+	} else {
+		call_encode(args, image, data)
+	}
+}
+
+func call_encode(args Arguments, image image.Image, data chan byte) {
+	//ENCODE or get INFO
+	//
+	//Size check -------------------------
+
+	//Open the target file
+	file, err := os.Open(args.target_path)
+	if err != nil {
+		panic(fmt.Sprintf("Error while opening %s", args.target_path))
+	}
+	defer file.Close()
+
+	// get the file size
+	stat, err := file.Stat()
+	if err != nil {
+		panic(fmt.Sprintf("Error while getting stats of file: %s", args.target_path))
+	}
+
+	rect := image.Bounds()
+
+	var isOk bool
+	var container_space int64
+	if args.use_full_byte {
+		container_space = int64(rect.Dx()*rect.Dy()) * 2
+
+	} else {
+		container_space = int64(rect.Dx() * rect.Dy())
+	}
+
+	isOk = stat.Size() < container_space
+
+	fmt.Println(stat.Size()/1000, "KB\\", container_space/1000, "KB")
+
+	if !isOk {
+		panic(fmt.Sprintf("Target file: %s does not fit into image file: %s", args.target_path, args.image_path))
+	}
+
+	if args.only_info {
+		return
+	}
+
+	if isOk {
+		fmt.Println("Encoding")
+
+		go scan(file, stat.Size(), data)
+
+		encapsulate(image, uint8(args.terminator), args.use_full_byte, args.align, data)
+	}
+}
+
+func call_decode(args Arguments, image image.Image, data chan byte) {
+	target_file, err := os.Create(args.target_path)
+	if err != nil {
+		panic(fmt.Sprintf("Error while opening %s for writing.", args.target_path))
+	}
+
+	fmt.Println("Decode")
+
+	signal := make(chan bool)
+
+	go writeToFile(target_file, data, signal)
+
+	decode(image, args.use_full_byte, args.align, data)
+
+	terminated := <-signal
+
+	if !terminated {
+		panic("There were errors writing the output file")
+	}
+}
+
+type Arguments struct {
+	image_path    string
+	target_path   string
+	wants_decode  bool
+	use_full_byte bool
+	align         rune
+	terminator    uint
+	only_info     bool
+}
+
+func parse_command_args() Arguments {
 	//FLAG READING
 	var full_byte, onlyInfo, wants_decode bool
 	var align rune
@@ -47,107 +153,16 @@ func main() {
 	case str_var == "l":
 		align = 'l'
 	default:
-		fmt.Println("Wrong align parameter")
-		return
+		panic("Wrong align parameter")
 	}
 
 	if image_path == "" || target_path == "" {
-		fmt.Println("You must specify a valid path for both image and target")
-		return
+		panic("You must specify a valid path for both image and target")
 	}
 
 	if terminator > 255 || terminator == 0 {
-		fmt.Println("Wrong terminator, must be in range 1-255")
-		return
+		panic("Wrong terminator, must be in range 1-255")
 	}
 
-	//
-
-	data := make(chan byte)
-
-	//Read the Png
-	image_file, err := os.Open(image_path)
-	if err != nil {
-
-		return
-	}
-	defer image_file.Close()
-
-	image, err := png.Decode(image_file)
-	if err != nil {
-		fmt.Println("Error while decoding png!")
-		return
-	}
-
-	if !wants_decode {
-		//ENCODE or get INFO
-		//
-		//Size check -------------------------
-
-		//Open the target file
-
-		file, err := os.Open(target_path)
-		if err != nil {
-			fmt.Println("Error while opening", target_path)
-			return
-		}
-		defer file.Close()
-
-		// get the file size
-		stat, err := file.Stat()
-		if err != nil {
-			return
-		}
-
-		rect := image.Bounds()
-
-		var isOk bool
-		var container_space int64
-		if full_byte {
-			container_space = int64(rect.Dx()*rect.Dy()) * 2
-
-		} else {
-			container_space = int64(rect.Dx() * rect.Dy())
-		}
-
-		isOk = stat.Size() < container_space
-
-		fmt.Println(stat.Size()/1000, "KB\\", container_space/1000, "KB")
-
-		if onlyInfo {
-			return
-		}
-
-		if isOk {
-			fmt.Println("Encoding")
-
-			go scan(file, stat.Size(), data)
-
-			encapsulate(image, uint8(terminator), full_byte, align, data)
-
-		}
-	} else {
-		// DECODE
-
-		target_file, err := os.Create(target_path)
-		if err != nil {
-			fmt.Println("Error while opening", target_path, "for writing.")
-			return
-		}
-
-		fmt.Println("Decode")
-
-		signal := make(chan bool)
-
-		go writeToFile(target_file, data, signal)
-
-		decode(image, full_byte, align, data)
-
-		terminated := <-signal
-
-		if !terminated {
-			fmt.Println("There were errors writing the output file")
-		}
-
-	}
+	return Arguments{image_path, target_path, wants_decode, full_byte, align, terminator, onlyInfo}
 }
